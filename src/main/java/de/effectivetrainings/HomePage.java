de3e17d0cdc4b7190b83e6a4dd7d147c38e264ba
dilgerm <martin@effectivetrainings.de>
2013-04-27 15:02:54 +0200

    First satisfied Dependency is injected in HomePage

--- src/main/java/de/effectivetrainings/HomePage.java
+++ src/main/java/de/effectivetrainings/HomePage.java
@@ -1,18 +1,30 @@
 package de.effectivetrainings;
 
-import org.apache.wicket.request.mapper.parameter.PageParameters;
-import org.apache.wicket.markup.html.basic.Label;
+import de.effectivetrainings.products.Product;
+import de.effectivetrainings.products.ProductProvider;
 import org.apache.wicket.markup.html.WebPage;
+import org.apache.wicket.markup.html.basic.Label;
+import org.apache.wicket.markup.html.list.ListItem;
+import org.apache.wicket.markup.html.list.ListView;
+import org.apache.wicket.model.PropertyModel;
+import org.apache.wicket.request.mapper.parameter.PageParameters;
+
+import javax.inject.Inject;
 
 public class HomePage extends WebPage {
 	private static final long serialVersionUID = 1L;
 
+    @Inject
+    ProductProvider provider;
+
 	public HomePage(final PageParameters parameters) {
 		super(parameters);
-
-		add(new Label("version", getApplication().getFrameworkSettings().getVersion()));
-
-		// TODO Add your page's components here
-
+        add(new ListView<Product>("productsList", provider.productList()) {
+            @Override
+            protected void populateItem(ListItem<Product> item) {
+                item.add(new Label("name", new PropertyModel<String>(item.getModel(),"name")));
+                item.add(new Label("prize", new PropertyModel<Double>(item.getModel(),"prize")));
+            }
+        });
     }
 }
