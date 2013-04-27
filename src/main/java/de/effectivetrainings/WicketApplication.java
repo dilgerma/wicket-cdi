6eb7a81549a9138f586fc49b13e434457e77f033
dilgerm <martin@effectivetrainings.de>
2013-04-27 14:25:49 +0200

    configure wicket and cdi

--- src/main/java/de/effectivetrainings/WicketApplication.java
+++ src/main/java/de/effectivetrainings/WicketApplication.java
@@ -1,7 +1,11 @@
 package de.effectivetrainings;
 
+import org.apache.wicket.cdi.CdiConfiguration;
 import org.apache.wicket.markup.html.WebPage;
 import org.apache.wicket.protocol.http.WebApplication;
+import org.jboss.weld.environment.servlet.Listener;
+
+import javax.enterprise.inject.spi.BeanManager;
 
 /**
  * Application object for your web application. If you want to run this application without deploying, run the Start class.
@@ -26,7 +30,12 @@ public class WicketApplication extends WebApplication
 	public void init()
 	{
 		super.init();
-
-		// add your configuration here
+        initCDI();
 	}
+
+    private void initCDI(){
+        BeanManager manager = (BeanManager)getServletContext().getAttribute(
+            Listener.BEAN_MANAGER_ATTRIBUTE_NAME);
+        new CdiConfiguration(manager).configure(this);
+    }
 }
